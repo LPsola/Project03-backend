6fd1721d4f695712621d389b2b606b9abdf4bca6
LPsola <leo.punsola@gmail.com>
2018-05-14 19:00:30 +0200

    nothing new here technically

--- routes/cards-api-router.js
+++ routes/cards-api-router.js
@@ -1,65 +1,18 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const projectRouter = express.Router();
-const Card = require("../../models/Card");
+const cardsRouter = express.Router();
+const Card = require("./../models/Card");
 const Project = require("./../models/Project");
-const List = require("../../models/List");
+const List = require("./../models/List");
 const User = require("./../models/User");
 
 // --------------------------------------------------
 // ROUTES
 // --------------------------------------------------
 
-// GET Cards LIST
-// -----------------
-projectRouter.get("/:userId/tasks", (req, res, next) => {
-  Project.find()
-    .then(projects => {
-      res.json(projects);
-    })
-    .catch(err => {
-      next(err);
-    });
-});
-
-// POST NEW PROJECT
-// ----------------
-projectRouter.post("/projects", (req, res, next) => {
-  const {
-    owner,
-    name,
-    imageUrl,
-    githubRepoUrl,
-    trelloBoardId,
-    slackWorkSpaceDirectory,
-    slackUserId,
-    contributors,
-    activityFeed
-  } = req.body;
-
-  Project.create({
-    owner,
-    name,
-    imageUrl,
-    githubRepoUrl,
-    trelloBoardId,
-    slackWorkSpaceDirectory,
-    slackUserId,
-    contributors,
-    activityFeed
-  })
-    .then(newProject => {
-      console.log(req.body);
-      res.json(newProject);
-    })
-    .catch(err => {
-      next(err);
-    });
-});
-
-// GET ONE PROJECT
-// ---------------
-projectRouter.get("/project/:projectId", (req, res, next) => {
+// GET list of Lists for one Project
+cardsRouter.get("/project/:projectId/lists", (req, res, next) => {
+  const listArray = [];
   if (!mongoose.Types.ObjectId.isValid(req.params.projectId)) {
     next();
     return;
@@ -70,31 +23,140 @@ projectRouter.get("/project/:projectId", (req, res, next) => {
       if (!project) {
         next();
         return;
+      } else {
+        List.find().then(list => {
+          list.forEach(oneList => {
+            if (oneList.projectId.toString() === project._id.toString()) {
+              listArray.push(oneList);
+            }
+          });
+          res.json(listArray);
+        });
       }
-      res.json(project);
     })
     .catch(err => {
       next(err);
     });
 });
 
-// POST ADD USER TO PROJECT
-// ------------------------
-// 1. search users from the database, according to the info passed in the form
-// 2. if a user matches, res.json
-projectRouter.get("/invite-contributor/:username", (req, res, next) => {
-  User.find({ username: req.params.username })
-    .then(user => {
-      res.json(user);
+// GET Cards LIST for one List of one Project
+// -----------------
+cardsRouter.get("/project/:projectId/:listId/cards", (req, res, next) => {
+  const cardsArray = [];
+  if (!mongoose.Types.ObjectId.isValid(req.params.projectId)) {
+    next();
+    return;
+  }
+
+  Project.findById(req.params.projectId)
+    .then(project => {
+      if (!project) {
+        next();
+        return;
+      } else {
+        if (!mongoose.Types.ObjectId.isValid(req.params.listId)) {
+          next();
+          return;
+        } else {
+          List.findById(req.params.listId).then(list => {
+            if (!list) {
+              next();
+              return;
+            } else if (list.projectId.toString() === project._id.toString()) {
+              Card.find().then(card => {
+                card.forEach(oneCard => {
+                  if (oneCard.listId.toString() === list._id.toString()) {
+                    cardsArray.push(oneCard);
+                  }
+                });
+                res.json(cardsArray);
+              });
+            } else {
+              next();
+              return;
+            }
+          });
+        }
+      }
     })
     .catch(err => {
       next(err);
     });
 });
 
-// PUT ONE PROJECT
-// DELETE ONE PROJECT
+// // POST NEW PROJECT
+// // ----------------
+// cardsRouter.post("/projects", (req, res, next) => {
+//   const {
+//     owner,
+//     name,
+//     imageUrl,
+//     githubRepoUrl,
+//     trelloBoardId,
+//     slackWorkSpaceDirectory,
+//     slackUserId,
+//     contributors,
+//     activityFeed
+//   } = req.body;
 
-// --------------------------------------------------
+//   Project.create({
+//     owner,
+//     name,
+//     imageUrl,
+//     githubRepoUrl,
+//     trelloBoardId,
+//     slackWorkSpaceDirectory,
+//     slackUserId,
+//     contributors,
+//     activityFeed
+//   })
+//     .then(newProject => {
+//       console.log(req.body);
+//       res.json(newProject);
+//     })
+//     .catch(err => {
+//       next(err);
+//     });
+// });
 
-module.exports = projectRouter;
+// // GET ONE PROJECT
+// // ---------------
+// cardsRouter.get("/project/:projectId", (req, res, next) => {
+//   if (!mongoose.Types.ObjectId.isValid(req.params.projectId)) {
+//     next();
+//     return;
+//   }
+
+//   Project.findById(req.params.projectId)
+//     .then(project => {
+//       if (!project) {
+//         next();
+//         return;
+//       }
+//       res.json(project);
+//     })
+//     .catch(err => {
+//       next(err);
+//     });
+// });
+
+// // POST ADD USER TO PROJECT
+// // ------------------------
+// // 1. search users from the database, according to the info passed in the form
+// // 2. if a user matches, res.json
+// cardsRouter.get("/invite-contributor/:username", (req, res, next) => {
+//   User.find({ username: req.params.username })
+//     .then(user => {
+//       res.json(user);
+//     })
+//     .catch(err => {
+//       next(err);
+//     });
+// });
+
+// // PUT ONE PROJECT
+// // DELETE ONE PROJECT
+
+// // --------------------------------------------------
+
+module.exports = cardsRouter;
