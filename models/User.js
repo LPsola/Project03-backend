9858d3a78bddf0a51d30b11e626338404fc63c4f
Rod <greasytaco@live.com>
2018-05-15 21:28:36 +0200

    Revert "Merge branch 'master' of https://github.com/LPsola/Project03-backend"
    This reverts commit ee618dd1b459295bbd5302a3b21ce6b40a7030a7, reversing
    changes made to 90266b80492c1415ed4513de4da0e74814d52984.

--- models/User.js
+++ models/User.js
@@ -7,13 +7,12 @@ const userSchema = new Schema(
     password: { type: String },
     email: { type: String },
     githubID: { type: String },
-    currentCards: { type: Object },
-    archivedCards: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: "archivedCards"
-      }
-    ]
+    currentCards: [{
+      type: Schema.Types.ObjectId,
+      ref: 'Cards'}],
+    archivedCards: [{ 
+      type: Schema.Types.ObjectId,
+      ref: 'Cards'}],
   },
   {
     timestamps: {
